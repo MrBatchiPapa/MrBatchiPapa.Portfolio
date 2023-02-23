161f91534b43681533f4808e2e1be8a81d53dcbe
MrBatchipapa <kevink314159@gmail.com>
2023-02-23 18:01:46 +1300

    feat: Improved about me section

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -2,9 +2,14 @@ import React, { useEffect } from 'react'
 import './index.css'
 import Aos from "aos";
 import "aos/dist/aos.css";
-import { FaJava, FaJs, FaReact, FaPython, FaHtml5, FaCss3Alt, FaAndroid, FaGit, FaLinkedin, FaGithub } from 'react-icons/fa'
+import CountUp from 'react-countup';
+import { FaJava, FaJs, FaReact, FaPython, FaHtml5, FaCss3Alt, FaAndroid, FaGit, FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa'
 
 const About = () => {
+
+    // const [ref, inView] = useInView({
+    //     threshold: 0.5,
+    // });
 
     useEffect(() => {
         Aos.init({ duration: 500 });
@@ -19,10 +24,11 @@ const About = () => {
                 <div className='aboutText'>
                     <h1>A bit about myself</h1>
                     <br />
-                    <h3>I am a student at the University Of Auckland, beginning my third year of studies as a software engineer and aspiring to be a full stack developer. <br /><br />I find interest in machine learning and overcoming problems with a well engineered thought process.  </h3>
+                    <h3>I am a student at the University Of Auckland, beginning my third year of studies as a software engineer and aspiring to be a full stack developer. I love to keep myself active with sports like badminton, table tennis, kickboxing or simply just gaming. <br /><br />To me the most important part of a company is its work environment, as I believe a close knit and trusting team is a successful one.  </h3>
                     <div className='info'>
                         <FaLinkedin className="icons1 Linkedin" />
                         <FaGithub className="icons1 Github" />
+                        <FaInstagram className="icons1 Instagram" />
                     </div>
                 </div>
 
@@ -31,8 +37,6 @@ const About = () => {
             <div data-aos="fade-left" className='skillsBox'>
                 <div className='skillsText'>
                     <h1>My tech stack</h1>
-                    <br />
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur malesuada risus, in sodales velit mollis id. Maecenas sit amet faucibus tellus. Nullam a ligula et mauris maximus ornare fringilla non felis. Donec congue suscipit ex, id finibus diam. Nullam tincidunt elit non ex vulputate laoreet. Mauris cursus cursus ante blandit accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sed dapibus urna.</h3>
                     <div className='stack'>
                         <FaJava className="icons Java" />
                         <FaJs className="icons Js" />
@@ -44,6 +48,12 @@ const About = () => {
                         <FaGit className="icons Git" />
                     </div>
 
+                    <h1>My experience</h1>
+                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum efficitur malesuada risus, in sodales velit mollis id. Maecenas sit amet faucibus tellus. Nullam a ligula et mauris maximus ornare fringilla non felis. Donec congue suscipit ex, id finibus diam. Nullam tincidunt elit non ex vulputate laoreet. Mauris cursus cursus ante blandit accumsan. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum sed dapibus urna.</h3>
+
+                    {/* <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                        {inView ? <CountUp start={0} end={3} duration={2} /> : null}
+                    </div> */}
                 </div>
             </div>
 
