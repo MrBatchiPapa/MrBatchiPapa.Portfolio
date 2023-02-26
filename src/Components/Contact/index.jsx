319e3d00e3ee2ab07ce1a6811d079236bf920b41
MrBatchipapa <kevink314159@gmail.com>
2023-02-26 18:01:50 +1300

    feat: Added email contact form

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,10 +1,32 @@
-import React from "react";
 import './index.css'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_fvgbw1c', 'template_6h6ei9y', form.current, 'rNv2o_B-6L83tcCLh')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
     return (
         <section className='contact'>
-           <h1>hi</h1>
+            <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+            </form>
         </section>
 
     )
